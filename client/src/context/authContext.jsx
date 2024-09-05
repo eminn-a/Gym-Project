29bbef108c432417cf1096a15114089bac9dab6c
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-09-05 18:40:12 +0300

    context fixes

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,17 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { getUserData } from "../utils/utils";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
-  const checkUser = getUserData();
-  if (userData === null) {
+
+  useEffect(() => {
+    const checkUser = getUserData();
     if (checkUser) {
       setUserData(checkUser);
     }
-  }
+  }, []);
+
   const adminEmail = "admin@abv.bg";
+
   return (
     <UserContext.Provider
       value={{ userData, setUserData, isAdmin: userData?.email === adminEmail }}
