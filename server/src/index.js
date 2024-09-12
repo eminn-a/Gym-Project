5b72733d8397518aea28135685f78542fdee6d31
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-09-12 12:34:22 +0300

    cors fix

--- server/src/index.js
+++ server/src/index.js
@@ -14,11 +14,7 @@ const PORT = process.env.PORT || 3030;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(auth);
 app.use(routes);
 
