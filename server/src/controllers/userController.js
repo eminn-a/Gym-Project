a12c002ae0f4c2830d41698e6633a6e9be9b7e7f
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-09-28 20:15:35 +0300

    test

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -18,6 +18,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie("authCookie", result.refreshToken, {
       httpOnly: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
